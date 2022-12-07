ecec7a8d485a2737a90a5a8a41120b53d331d27b
ben romdhane ines <benromdhane2052@gmail.com>
2022-12-07 19:44:46 +0100

    put it togather

--- first-react-with-jsx/src/App.js
+++ first-react-with-jsx/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 
 
 function App() {
-  const todoList = ['Learn React', 'Climb Mt. Everest', 'Run a Marathon', 'Feed the dogs'];
   const personalData = [
     {
       firstName: 'Jane',
@@ -33,10 +32,6 @@ function App() {
   ]
   return (
     <div className="App">
-      <h1>Hello Dojo</h1>
-      <h2>things i need to do:</h2>
-      <ul>{todoList.map((todo,index) => <li key={index}>{todo}</li>)}</ul>
-      <hr/>
       {personalData.map((person,index) => <PersonalCard person={person} key={index}/>)}
     </div>
   );
--- first-react-with-jsx/src/personal-card/personal-card.js
+++ first-react-with-jsx/src/personal-card/personal-card.js
@@ -1,22 +1,15 @@
-import React,  { Component } from 'react';
-class PersonalCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        age: this.props.person.age
-    };
-}
-  render() { 
+import React,  { useState } from 'react';
+const PersonalCard = ({person}) => {
+  const [age, setAge] = useState(person.age)
     return (
       <div className="person-card">
-        <h3>{this.props.person.lastName}, {this.props.person.firstName}</h3>
-        <p>Age: {this.state.age}</p>
-        <p>Hair color: {this.props.person.hairColor}</p>
-        <button onClick={() => this.setState({age: this.state.age + 1}) }>
-          birthday button for {this.props.person.firstName} {this.props.person.lastName} 
+        <h3>{person.lastName}, {person.firstName}</h3>
+        <p>Age: {age}</p>
+        <p>Hair color: {person.hairColor}</p>
+        <button onClick={() => setAge(age + 1) }>
+          birthday button for {person.firstName} {person.lastName} 
         </button>
       </div>
-    );
-  }
+    )
 }
 export default PersonalCard;
