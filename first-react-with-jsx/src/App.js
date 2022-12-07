a64413aa927af97daa3dac0f7ca4328ccbb9d728
ben romdhane ines <benromdhane2052@gmail.com>
2022-12-07 20:57:15 +0100

    more-form

--- first-react-with-jsx/src/App.js
+++ first-react-with-jsx/src/App.js
@@ -5,32 +5,6 @@ import './App.css'
 
 
 function App() {
-  const personalData = [
-    {
-      firstName: 'Jane',
-      lastName: 'Doe',
-      age: 36,
-      hairColor: 'Black',
-    },
-    {
-      firstName: 'John',
-      lastName: 'Smith',
-      age: 88,
-      hairColor: 'Brown',
-    },
-    {
-      firstName: 'Millard',
-      lastName: 'FillMore',
-      age: 50,
-      hairColor: 'Brown',
-    },
-    {
-      firstName: 'Maria',
-      lastName: 'Smith',
-      age: 36,
-      hairColor: 'black',
-    },
-  ]
   return (
     <div className="app">
       <Form/>
--- first-react-with-jsx/src/Components/form.js
+++ first-react-with-jsx/src/Components/form.js
@@ -12,31 +12,29 @@ const Form = () => {
         <div className="form-item">
           <label for="first-name">First Name</label>
           <input type="text" id="first-name" className="form-control" onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
+          {firstName.length > 0 && firstName.length < 2 && <p>First Name must be at least 2 characters</p>}
         </div>
         <div className="form-item">
           <label for="last-name">Last Name</label>
           <input type="text" id="last-name" className="form-control" onChange={(e) => setLastName(e.target.value)} value={lastName}/>
+          {lastName.length > 0 && lastName.length < 2 && <p>Last Name must be at least 2 characters</p>}
         </div>
         <div className="form-item">
           <label for="email">Email</label>
           <input type="text" id="email" className="form-control" onChange={(e) => setEmail(e.target.value)} value={email}/>
+          {email.length > 0 && email.length < 5 && <p>Email must be at least 5 characters</p>}
         </div>
         <div className="form-item">
           <label for="password">Password</label>
           <input type="text" id="password" className="form-control" onChange={(e) => setPassword(e.target.value)} value={password}/>
+          {password.length > 0 && password.length < 5 && <p>Password must be at least 8 characters</p>}
         </div>
         <div className="form-item">
           <label for="confirm-password">Password</label>
           <input type="text" id="confirm-password" className="form-control" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/>
+          {(confirmPassword.length < 0 || password !== confirmPassword) && <p>Passwords must match</p>}
         </div>
       </form>
-
-      <h2>Your form data</h2>
-      <p>First Name: {firstName}</p>
-      <p>Last Name: {lastName}</p>
-      <p>Email: {email}</p>
-      <p>Password: {password}</p>
-      <p>Confirm Password: {confirmPassword}</p>
     </div>
   )
 }
